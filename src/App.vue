861c856a1a889de713cf206800456b8a3b9f0488
Jaeiya <exanix@gmail.com>
2018-08-01 16:25:24 -0700

    Initial: Setup Vue Environment
    Working webapp using Vue CLI-3, with some compartmentalized Single-File-Components ref #5

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Chickens</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss" src='./app.scss'></style>
