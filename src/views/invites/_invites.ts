ecf25ce7678e6471cb0188886e6615414ef1f5c3
Jaeiya <exanix@gmail.com>
2018-11-05 18:02:59 -0800

    Add a few invite controls ref #1

--- src/views/invites/_invites.ts
+++ src/views/invites/_invites.ts
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import MySelect from '@/components/elements/MySelect.vue';
 
-@Component
+@Component({
+  components: {
+    MySelect
+  }
+})
 export default class Invites extends Vue {
 
+  public uses = ['1', '5', '10', '100', 'Infinite'];
+  public days = ['1', '7', '30', '365', 'Infinite'];
 
-
-  public created() {  }
+  public created() { console.log('hello world'); }
 
 }
 
