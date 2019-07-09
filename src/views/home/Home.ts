1edbfa9419902eb0af6f35a910b660b6c21a8332
Jaeiya <exanix@gmail.com>
2019-07-09 16:51:55 -0700

    Fix home page

--- src/views/home/Home.ts
+++ src/views/home/Home.ts
@@ -1,14 +1,7 @@
-import { Component, Vue } from 'vue-property-decorator';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import * as moment from 'moment';
 
-// @Component({
-//   components: {
-//     HelloWorld,
-//   },
-//   created: () => {
-//     console.dir(moment);
-//   },
-// })
-
+@Component
 export default class Home extends Vue {}
